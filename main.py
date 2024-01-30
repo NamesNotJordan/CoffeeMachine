06e7db5d055395cc3a734ef3b5f0de267bd4fe23
Jayden Hendrickse <jayden.herberhendrickse@gmail.com>
2024-01-30 21:28:05 +0200

    Added main and money resource
    Signed-off-by: Jayden Hendrickse <jayden.herberhendrickse@gmail.com>

--- main.py
+++ main.py
@@ -28,13 +28,27 @@ resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
+    "money": 0.0,
 }
 
 
 def report_resources():
-    for key in resources:
-        print(f"{key}: {resources[key]}")
+    print(f"water")
 
 
 def check_resources_for_drink(drink):
     return drink['water'] <= resources['water'] & drink['milk'] <= resources['milk'] & drink['coffee'] <= resources['coffee']
+
+
+while True:
+    choice = input("What can I get you, Friend? (espresso/latte/cappuccino):")
+    if choice == 'off':
+        break
+    elif choice == 'report':
+        report_resources()
+    elif choice == 'espresso':
+        pass
+    elif choice == 'latte':
+        pass
+    elif choice == 'cappuccino':
+        pass
